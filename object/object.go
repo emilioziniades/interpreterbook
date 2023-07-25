50be89f3e9581a9471b08703b38f919d14fda5a7
Emilio Ziniades <emilioziniades@protonmail.com>
2023-07-25 23:20:14 +0200

    evaluate functions

--- object/object.go
+++ object/object.go
@@ -3,6 +3,9 @@ package object
 import (
 	"fmt"
 	"strconv"
+	"strings"
+
+	"github.com/emilioziniades/interpreterbook/ast"
 )
 
 type ObjectType string
@@ -13,6 +16,7 @@ const (
 	NULL_OBJ         ObjectType = "NULL"
 	RETURN_VALUE_OBJ ObjectType = "RETURN_VALUE"
 	ERROR_OBJ        ObjectType = "ERROR"
+	FUNCTION_OBJ     ObjectType = "FUNCTION"
 )
 
 type Object interface {
@@ -52,3 +56,28 @@ type Error struct {
 
 func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
+func (f *Function) Inspect() string {
+	var out strings.Builder
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
